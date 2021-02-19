a0528d659810bfcc2c680c9c6deb74ab34ee37a2
Danielle Klaasen <danielleklaasen@gmail.com>
2021-02-19 12:20:16 +0100

    Add Native base and webpack configuration

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Container, Text } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Container style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    </Container>
   );
 }
 
